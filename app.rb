12b3dbe5dac7f876265c75ec1d36fd16d828a953
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-11-11 07:04:17 -0500

    working code to retrieve access token

--- app.rb
+++ app.rb
@@ -7,11 +7,15 @@ class Token
   base_uri "https://github.com"
   
   def initialize(code)
-    @access_token = self.class.post("/login/oauth/access_token", {
+    params = {
       :client_id => "148718244dbf91ab58bc",
       :client_secret => "e065813ece4e5738c0a37aa7b11e5da7db4d27bf",
       :code => code
-    })
+    }
+    query = "?client_id=" + params[:client_id] + "&client_secret=" + params[:client_secret] + "&code=" + params[:code]
+    @access_token = self.class.post("/login/oauth/access_token" + query)
+
+    #access_token=X&type_bearer=Y
   end
   
   def to_s
@@ -27,5 +31,4 @@ get '/follow/:user' do
   user = params[:user]
   code = params[:code]
   token = Token.new code
-  logger.info "USER: #{user}, CODE: #{code}, ACCESS TOKEN: #{token.to_s}"
 end
