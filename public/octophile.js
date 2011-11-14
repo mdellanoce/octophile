86c3929ab0636dedd5f133cc67549e0dcd4867ee
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-11-13 19:57:44 -0500

    live update the button preview

--- public/octophile.js
+++ public/octophile.js
@@ -5,10 +5,19 @@
     return false;
   });
 
-  $('#username').keyup(function() {
-    var user = $(this).val();
-    $('#html').val(
-      '<a href="https://github.com/' + user + '" class="github-follow-button">Follow On GitHub</a>\n' +
-      '<script type="text/javascript" src="http://octophile.com/widgets.js"></script>');
-  }).keyup();
+  var username = $('#username'),
+    user = username.val();
+  setInterval(function() {
+    if (user !== username.val()) {
+      user = username.val();
+
+      $('#html').val(
+        '<a href="https://github.com/' + user + '" class="github-follow-button">Follow On GitHub</a>\n' +
+        '<script type="text/javascript" src="http://octophile.com/widgets.js"></script>');
+
+      $('iframe').attr('src', function(i, src) {
+        return src.replace(/user=(.+)/, 'user=' + user);
+      });
+    }
+  }, 250);
 })(jQuery);
